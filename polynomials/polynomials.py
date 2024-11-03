d7ce53cc5d06f7629f5398bd3dc591875b05d308
Rehan-Baig1294 <rehanbaig12@outlook.com>
2024-11-03 22:50:50 +0000

    first poly made

--- polynomials/polynomials.py
+++ polynomials/polynomials.py
@@ -1,9 +1,9 @@
-from numbers import Number
+from numbers import Number # noqa f401
 
 
 class Polynomial:
+    """Using polynomials."""
 
     def __init__(self, coefs):
+        """Initialise method."""
         self.coefficients = coefs
-
-
