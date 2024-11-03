238784ce08d736c75ba01883e4f3cb0536fcbfd2
Rehan-Baig1294 <rehanbaig12@outlook.com>
2024-11-03 23:47:26 +0000

    got polynomials to print

--- polynomials/polynomials.py
+++ polynomials/polynomials.py
@@ -7,3 +7,22 @@ class Polynomial:
     def __init__(self, coefs):
         """Initialise method."""
         self.coefficients = coefs
+
+    def degree(self):
+        """Determine the degree of polynomial."""
+        return (len(self.coefficients) - 1)
+    
+    def __str__(self): 
+        lizst = list(self.coefficients) 
+        tring = ""
+        for i in range(len(lizst)):
+            coef = str(lizst[i])
+            if lizst[i] != 0:
+                if i == 0:
+                    tring = tring + coef
+                elif i == 1:
+                    tring = tring + " + " + coef + "x"
+                else:
+                    tring = tring + " + " + coef + "x^" + str(i)
+        return tring
+            
