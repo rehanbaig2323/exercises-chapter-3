d6fd59bf61fa2e987d46b8239ad53250abee23fc
Rehan-Baig1294 <rehanbaig12@outlook.com>
2024-11-05 20:35:46 +0000

    gooo

--- shape/circle.py
+++ shape/circle.py
@@ -5,7 +5,8 @@ class Circle:
         self.radius = radius 
 
     def __contains__(self, point): 
-        return ((self.centre[0] - point[0]) ** 2 + (self.centre[1] - point[1]) ** 2 < self.radius)
+        print 
+        return ((self.centre[0] - point[0]) ** 2 + (self.centre[1] - point[1]) ** 2 < (self.radius) ** 2)
     
-c = Circle((1., 0.), 2)
-print((0.5, 0.5) in c)
+c = Circle((11.2, -5.1), 2.8)
+print((10.2, -3.11) in c)
